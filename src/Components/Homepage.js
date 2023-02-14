0f2db3b5789b1112dfc2aa749d8636d4367bc7eb
Simon Y <therealkjai139@gmail.com>
2023-02-13 19:53:18 -0500

    fontpage

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -2,13 +2,25 @@ import { useState } from "react"
 import homePhoneScreen from "./Assets/images/home-phones.png"
 import phone1 from "./Assets/images/phone1.png"
 import phone2 from "./Assets/images/phone2.png"
-
+import appleBtn from "./Assets/images/apple-logo.png"
+import googleBtn from "./Assets/images/google-logo.png"
+import googleLogo from "./Assets/images/google-icon.png" 
 
 const Homepage = () => {
 
     const [bgImg, setBgImg] = useState(phone1)
 
+    const [isPwDisplayed, setIsPwDisplayed] = useState('password')
 
+
+    const togglePwDisplay = (e) => {
+        e.preventDefault()
+        if (isPwDisplayed === 'password') {
+            setIsPwDisplayed('text')
+        } else {
+            setIsPwDisplayed('password')
+        }
+    }
 
     return (
         <div className="homePage">
@@ -27,9 +39,24 @@ const Homepage = () => {
                         Instagram
                     </div>
                     <form className="homeLoginForm">
-                        <input type="text" name="userName"></input>
-                        <input type="text" name="userPw"></input>
-                        <button>Log in</button>
+                        <label>
+                        <input type="text" name="userName" placeholder=" " className="formInput"></input>
+                        <span className="placeHolderTxt">Phone number, username or email address</span>
+                        </label>
+                        
+
+                        <label>
+                        <input type={isPwDisplayed} name="userPassword" placeholder=" " className="formInput"></input>
+                        <span className="placeHolderTxt">
+                            Password
+                            
+
+                        </span>
+                        <button className="pwToggleBtn" onClick={togglePwDisplay}>Show</button>
+                        
+                        </label>
+                        
+                        <button className="logInBtn">Log in</button>
 
                     </form>
                     <div className="orDiv">
@@ -38,9 +65,11 @@ const Homepage = () => {
                         <div className="orRight"></div>
                     </div>
                     <div className="centerT">
-                        Log in with Google
+                        <button className="googleBtn"><div className="googleLogo" style={{
+                            backgroundImage: `url(${googleLogo})`
+                        }}></div><div>Log in with Google</div></button>
                     </div>
-                    <div className="centerT">
+                    <div className="centerT fPwDiv">
                         Forgotten your password?
                     </div>
                 </div>
@@ -49,11 +78,16 @@ const Homepage = () => {
                         <p>Don't have an account?</p><p className="boldT">Sign up</p>
                     </div>
                 </div>
-                <div className="centerT">
+                <div className="centerT getAppTxt">
                     Get the app.
                 </div>
                 <div className="downloadLogo">
-                    
+                    <button className="dlLogo" style={{
+                        backgroundImage: `url(${appleBtn})`
+                    }}></button>
+                    <button className="dlLogo" style={{
+                        backgroundImage: `url(${googleBtn})`
+                    }}></button>
                 </div>
             </div>
         </div>
