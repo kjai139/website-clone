3b9f82c2383973c086b62011754047b4f43a7b63
simon y <71997308+kjai139@users.noreply.github.com>
2023-03-07 22:13:56 -0500

    headers bug

--- src/Components/UserDashboard.js
+++ src/Components/UserDashboard.js
@@ -542,7 +542,8 @@ const UserDashboard = () => {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
-              'Authorization':"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"
+              'Authorization':"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",
+              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
             },
             body: JSON.stringify({
                 "prompt": promptMsg,
