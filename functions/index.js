644c94153aaea44ac5702374c75989e90b5e64a1
simon y <71997308+kjai139@users.noreply.github.com>
2023-03-08 17:58:12 -0500

    fsaf

--- functions/index.js
+++ functions/index.js
@@ -2,30 +2,22 @@ const functions = require("firebase-functions");
 const {Configuration, OpenAIApi} = require("openai");
 
 
-exports.createImage = functions.https.onCall( async (data, context) => {
+exports.createImage = functions.https.onCall(async (data, context) => {
   const prompt = data.prompt;
-
-  const response = {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "POST",
-      "Access-Control-Allow-Headers": "Content-Type",
-
-    },
-  };
 
   const configuration = new Configuration({
     apiKey: process.env.REACT_APP_OPENAI_API,
   });
   const openai = new OpenAIApi(configuration);
 
-  const apiResponse = await openai.createImage({
+  return openai.createImage({
     prompt: prompt,
     n: 3,
     size: "512x512",
+  }).then( (response) => {
+    const imageURLS = response.data;
+    return imageURLS;
   });
-  response.results = apiResponse.data;
-  return response;
 });
 
 
